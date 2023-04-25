7f834e89d2c662ebf498d3e9197e29b8e0487bdf
Roland.yin <rolandyin.ustc@gmail.com>
2023-04-25 13:55:00 +0300

    11

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,10 +2,17 @@ $(document).ready(function() {
     $('a.abstract').click(function() {
         $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
         $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
+        $(this).parent().parent().find(".video.hidden.open").toggleClass('open');
     });
     $('a.bibtex').click(function() {
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
         $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
+        $(this).parent().parent().find(".video.hidden.open").toggleClass('open');
+    });
+    $('a.video').click(function() {
+        $(this).parent().parent().find(".video.hidden").toggleClass('open');
+        $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
+        $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
     });
     $('a').removeClass('waves-effect waves-light');
 });
